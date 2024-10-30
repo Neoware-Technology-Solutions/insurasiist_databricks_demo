f0c6a6b41df3460a6c1aadce57b28368d90c203a
SURYA GAYATHRY <131143004+Suryaachuth@users.noreply.github.com>
2024-10-30 12:27:37 +0530

    Update databricks_poicy.py

--- src/databricks_poicy.py
+++ src/databricks_poicy.py
@@ -1,3 +1,22 @@
+
+__import__("pysqlite3")
+import sys
+import os
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join("./", "db.sqlite3"),
+    }
+}
+
+
+
+
+
+
 import chromadb
 import openai
 from dotenv import load_dotenv
